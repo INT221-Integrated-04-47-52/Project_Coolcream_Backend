53bb5a834c8dfb3af3db33bff858a4959e0a1e0a
Noochajee Phonbooncharoenchai <noochajee.nn@mail.kmutt.ac.th>
2021-05-04 01:43:42 +0700

    Add Product model

---
+++ src/main/java/sit/int221/coolcream/models/Product.java
@@ -0,0 +1,106 @@
+package sit.int221.coolcream.models;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="product")
+public class Product {
+
+  @Id
+  @Column(name="product_id")
+  private long productId;
+  @Column(name="product_name")
+  private String productName;
+  private String describe;
+  private double price;
+  private java.sql.Date lastday;
+  private String image;
+  @ManyToOne
+  @JoinColumn(name="brand_brand_id")
+  private Brand brand;
+  @ManyToOne
+  @JoinColumn(name="size_size_id")
+  private Size size;
+//  @OneToMany(mappedBy = "product")
+//  private List<ProductHasTopping> productTopping;
+
+
+  public long getProductId() {
+    return productId;
+  }
+
+  public void setProductId(long productId) {
+    this.productId = productId;
+  }
+
+
+  public String getProductName() {
+    return productName;
+  }
+
+  public void setProductName(String productName) {
+    this.productName = productName;
+  }
+
+
+  public String getDescribe() {
+    return describe;
+  }
+
+  public void setDescribe(String describe) {
+    this.describe = describe;
+  }
+
+
+  public double getPrice() {
+    return price;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+
+  public java.sql.Date getLastday() {
+    return lastday;
+  }
+
+  public void setLastday(java.sql.Date lastday) {
+    this.lastday = lastday;
+  }
+
+
+  public String getImage() {
+    return image;
+  }
+
+  public void setImage(String image) {
+    this.image = image;
+  }
+
+
+  public Brand getBrand() {
+    return brand;
+  }
+
+  public void setBrand(Brand brand) {
+    this.brand = brand;
+  }
+
+  public Size getSize() {
+    return size;
+  }
+
+  public void setSize(Size size) {
+    this.size = size;
+  }
+
+//  public List<ProductHasTopping> getProductTopping() {
+//    return productTopping;
+//  }
+//
+//  public void setProductTopping(List<ProductHasTopping> productTopping) {
+//    this.productTopping = productTopping;
+//  }
+}
