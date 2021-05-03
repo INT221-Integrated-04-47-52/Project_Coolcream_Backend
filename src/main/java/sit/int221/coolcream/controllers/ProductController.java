b6ff29ea2007d1ee44f507713efe16adb03a6d7f
Noochajee Phonbooncharoenchai <noochajee.nn@mail.kmutt.ac.th>
2021-05-04 01:36:38 +0700

    Update ProductController

--- src/main/java/sit/int221/coolcream/controllers/ProductController.java
+++ src/main/java/sit/int221/coolcream/controllers/ProductController.java
@@ -1,8 +1,7 @@
 package sit.int221.coolcream.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sit.int221.coolcream.models.Product;
 import sit.int221.coolcream.repositories.ProductRepository;
 
@@ -15,6 +14,27 @@ public class ProductController {
 
     @GetMapping ("/product")
     public List<Product> product() {
+        return productRepository.findAll();
+    }
+
+//    @GetMapping ("/show/{product_id}")
+//    public Product show (@PathVariable Long product_id){
+//        return productRepository.findById(product_id).orElse(null);
+//    }
+
+    @PostMapping("/add")
+    public Product add (@RequestBody Product product){
+        return productRepository.save(product);
+    }
+
+    @PutMapping("/edit/{product_id}")
+    public List<Product> add (@PathVariable Long product_id){
+        return productRepository.findAll();
+    }
+
+    @DeleteMapping("/delete/{product_id}")
+    public List<Product> delete(@PathVariable Long product_id){
+        productRepository.deleteById(product_id);
         return productRepository.findAll();
     }
 }
