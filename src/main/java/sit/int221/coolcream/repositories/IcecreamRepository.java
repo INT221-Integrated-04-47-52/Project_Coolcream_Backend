c0260b95e72de2a5d6b37b748029bf045e383cf4
Noochajee Phonbooncharoenchai <noochajee.nn@mail.kmutt.ac.th>
2021-05-05 22:30:58 +0700

    Update IcecreamRepository method findByName

--- src/main/java/sit/int221/coolcream/repositories/IcecreamRepository.java
+++ src/main/java/sit/int221/coolcream/repositories/IcecreamRepository.java
@@ -4,8 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import sit.int221.coolcream.models.Icecream;
 
+
 public interface IcecreamRepository extends JpaRepository<Icecream, Long> {
 
     @Query(value = "SELECT max(icecreamId) FROM Icecream")
-    public Long getMaxIcecreamId();
+    Long getMaxIcecreamId();
+
+    @Query(value = "SELECT i FROM Icecream i WHERE i.icecreamName = ?1")
+    Icecream findByName(String icecreamName);
 }
