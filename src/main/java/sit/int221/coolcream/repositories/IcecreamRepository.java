36cbb2c1d79909c7ccbb2376e0d4cae805abe380
Noochajee Phonbooncharoenchai <noochajee.nn@mail.kmutt.ac.th>
2021-05-05 10:19:35 +0700

    Update IcecreamRepository getMaxIcecreamId

--- src/main/java/sit/int221/coolcream/repositories/IcecreamRepository.java
+++ src/main/java/sit/int221/coolcream/repositories/IcecreamRepository.java
@@ -1,9 +1,11 @@
 package sit.int221.coolcream.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import sit.int221.coolcream.models.Icecream;
 
 public interface IcecreamRepository extends JpaRepository<Icecream, Long> {
 
-
+    @Query(value = "SELECT max(icecreamId) FROM Icecream")
+    public Long getMaxIcecreamId();
 }
