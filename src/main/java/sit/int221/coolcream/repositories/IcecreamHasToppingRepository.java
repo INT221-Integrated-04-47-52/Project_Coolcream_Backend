41656a5fb30ba53b456f58af3ac615a25e9f1980
Noochajee Phonbooncharoenchai <noochajee.nn@mail.kmutt.ac.th>
2021-05-08 07:08:28 +0700

    Update IcecreamHasTopping datatype

--- src/main/java/sit/int221/coolcream/repositories/IcecreamHasToppingRepository.java
+++ src/main/java/sit/int221/coolcream/repositories/IcecreamHasToppingRepository.java
@@ -3,6 +3,6 @@ package sit.int221.coolcream.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sit.int221.coolcream.models.IcecreamHasTopping;
 
-public interface IcecreamHasToppingRepository extends JpaRepository<IcecreamHasTopping, String> {
+public interface IcecreamHasToppingRepository extends JpaRepository<IcecreamHasTopping, Long> {
 
 }
