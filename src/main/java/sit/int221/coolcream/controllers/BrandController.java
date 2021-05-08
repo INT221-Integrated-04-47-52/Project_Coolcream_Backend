4e40a60f3e1fc932224bc034b64c276c2ac1ced3
Noochajee Phonbooncharoenchai <noochajee.nn@mail.kmutt.ac.th>
2021-05-09 02:24:00 +0700

    Update Controller

--- src/main/java/sit/int221/coolcream/controllers/BrandController.java
+++ src/main/java/sit/int221/coolcream/controllers/BrandController.java
@@ -1,7 +1,6 @@
 package sit.int221.coolcream.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sit.int221.coolcream.models.Brand;
